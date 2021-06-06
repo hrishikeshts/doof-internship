00e01775a747e9beda953e8dfe6a6672f522b929
Hrishikesh Thesserikaran <hrishikeshts@gmail.com>
2021-06-06 12:51:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Title from "./TitleSVG";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Title />
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/TitleSVG.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TitleSVG() {
+    return (
+        <svg viewBox="0 0 100 25" xmlns="http://www.w3.org/2000/svg">
+            <text fill="#F7D832" font-family="Montserrat, sans-serif" font-weight="700" text-anchor="middle" font-size="16.1" x="50" y="15">
+                DOOF
+            </text>
+        </svg>
+    );
+}
